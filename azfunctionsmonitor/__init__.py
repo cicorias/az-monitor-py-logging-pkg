8ae9374c7964eab64346d0a6cc210509875cb48a
Shawn Cicoria <github@cicoria.com>
2021-03-07 15:01:45 -0500

    bump version

--- azfunctionsmonitor/__init__.py
+++ azfunctionsmonitor/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.111'
+__version__ = '0.1.112'
 
 from .LoggingHelper import LoggingHelper, get_logger
 
--- tests/test_az_monitor_py_logging.py
+++ tests/test_az_monitor_py_logging.py
@@ -3,4 +3,4 @@ from azfunctionsmonitor import __version__, get_logger
 
 def test_version():
     l = get_logger("foo")
-    assert __version__ == '0.1.111'
+    assert __version__ == '0.1.112'
